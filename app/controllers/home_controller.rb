de0f69e697041c32509b394c633df8c3db27a34f
Tao Zhang <tao.zhang@theeagleforce.net>
2014-11-14 01:13:44 -0500

    finish adding the click event in the appointment system.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,12 +8,68 @@ class HomeController < ApplicationController
 	end
 
 	def init_event
-		@event = Event.all
+		if params.has_key?(:start) and params.has_key?(:end)
+			start_day = params[:start].to_time
+			end_day = params[:end].to_time
+			event = Event.where(:reserve_start_at=>start_day..end_day)
+			result = []
+			event.map do |e|
+				t = e.product
+				te = e.employee
+				tc = e.customer
+				result.push e.attributes.slice("id","reserve_start_at","reserve_end_at","employee_id","customer_id","product_id").merge("product_name"=>t.product_name, "price"=>t.price,"employee_name"=>te.employee_name,"customer_name"=>tc.customer_name)
+			end
+		    respond_to do |format|
+		      format.json { 
+		        render json: result
+		      }
+		    end
+		end
+	end
+
+	def get_all_info
+		re_p = []
+		re_e = []
+		re_c = []
+		product = Product.all
+		employee = Employee.all
+		customer = Customer.all
+		result = []
+		product.map do |e|
+			re_p.push e.attributes.slice("id","product_name")
+		end
+		employee.map do |e|
+			re_e.push e.attributes.slice("id","employee_name")
+		end
+		customer.map do |e|
+			re_c.push e.attributes.slice("id","customer_name")
+		end
+		result.push re_p, re_e, re_c
 	    respond_to do |format|
 	      format.json { 
-	        render json: @event.map
+	        render json: result
 	      }
 	    end
+	end
+
+	def update_event
+		if params.has_key?(:id) and params.has_key?(:start) and params.has_key?(:product_id) and params.has_key?(:end) and params.has_key?(:employee_id) and params.has_key?(:customer_id)
+			temp = Event.find(params[:id].to_i)
+			begin
+				logger.debug(temp)
+				temp.update_attributes(:employee_id=>params[:employee_id].to_i, :customer_id=>params[:customer_id].to_i, :product_id=>params[:product_id].to_i)
+				logger.debug(params[:id])
+				temp.update_attributes(:reserve_start_at=>params[:start].to_time, :reserve_end_at=>params[:end].to_time)
+				result = "Success"
+			rescue
+				result = "Fail"
+			end
+			respond_to do |format|
+				format.json {
+					render json: {result: result}
+				}
+			end
+		end
 	end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ActiveRecord::Base
+
+    attr_accessible :image_address, :unit, :amount, :price, :product_name
+
 	has_many :event
 end
